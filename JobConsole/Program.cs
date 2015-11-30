446279df613cb290d17e68a92615ca4d0be6d3b3
EnchMan <sammyextra@gmail.com>
2015-11-30 23:43:18 +0100

    Update: Fixed database

--- JobConsole/Program.cs
+++ JobConsole/Program.cs
@@ -4,66 +4,114 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JobLib;
-
+using JobEngine;
+using System.Data.SqlClient;
+using System.Data;
 namespace JobConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Initialize();
+            //Initialize();
+            Run();
+        }
+
+        static void Run()
+        {
+            Console.WriteLine("Welcome to SQL Console");
+            Console.WriteLine("Press Any to continue");
+            while(Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                PrepareParam();
+            }
         }
 
         static void Initialize()
         {
-            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            do
             {
-                string sql = "[setEmployee]";
+                Console.Write("Query: ");
+                string sql = Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine(sql);
+
+                Console.Write("Firstname: ");
+                string firstname = Console.ReadLine();
+
+                Console.Write("Lastname: ");
+                string lastname = Console.ReadLine();
+
+                Console.Write("Phone: ");
+                string phone = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
                 Dictionary<string, object> param = new Dictionary<string, object> { };
-                param.Add("name", "Test Stored Procedure");
+                param.Add("firstname", firstname);
+                param.Add("lastname", lastname);
+                param.Add("phone", phone);
+                param.Add("email", email);
 
-                Database db = new Database(sql, param);
+                Db db = new Db(sql, param);
+                db.Execute(System.Data.CommandType.StoredProcedure);
 
-                Console.WriteLine("Inserted");
+                Console.Clear();
+                Console.WriteLine("Execute -> {0}", sql);
             }
-                
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
 
-            //while(Console.ReadKey().Key != ConsoleKey.Escape)
-            //{
-            //    //Console.Write("SQL QUERY: ");
-            //    //string sql = Console.ReadLine();
-            //    string sql = "INSERT INTO [employees] ([name]) VALUES(@name)";
-            //    Console.Write("QUERY: {0}", sql);
-            //    Dictionary<string, object> param = new Dictionary<string, object> { };
-            //    param.Add("name", "Sam Møller");
+        static void PrepareParam()
+        {
+            Console.WriteLine("Running SQL");
+            Dictionary<string, object> param = new Dictionary<string, object> { };
 
-            //    Database db = new Database(sql, param);
+            Console.Write("Query: ");
+            string sql = Console.ReadLine();
 
-            //    Console.WriteLine("Executed");
+            Console.Clear();
 
-            //    //List<Dictionary<string, object>> items = db.Fetch();
+            Console.WriteLine("Press Enter to add parameters");
 
-            //    //foreach(Dictionary<string, object> item in items)
-            //    //{
-            //    //    foreach(KeyValuePair<string, object> data in item)
-            //    //    {
+            while (Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Console.Write("Parameter name: ");
+                string name = Console.ReadLine();
+                Console.Write("Parameter value: ");
+                string value = Console.ReadLine();
 
-            //    //        Console.Write("{0}", data.Key);
-            //    //        Console.ForegroundColor = ConsoleColor.Gray;
-            //    //        Console.Write(":");
-            //    //        Console.ResetColor();
-            //    //        Console.ForegroundColor = ConsoleColor.Green;
-            //    //        Console.Write("{0}", data.Value.ToString());
-            //    //        Console.ResetColor();
-            //    //        Console.ForegroundColor = ConsoleColor.DarkRed;
-            //    //        Console.Write(";\t");
-            //    //        Console.ResetColor();
-            //    //    }
-            //    //    Console.WriteLine();
-            //    //}
+                Console.Clear();
+                param.Add(name, value);
+                Console.WriteLine("Press Enter to add more");
+            }
 
-            //    //Console.WriteLine("Inserted employee");
-            //}
+            Console.Clear();
+
+            Console.WriteLine("Want to run \"{0}\"", sql);
+            Console.WriteLine("Press ENTER to run");
+            if(Console.ReadKey().Key == ConsoleKey.Enter)
+            {
+                Db db;
+                if (param.Count != 0)
+                {
+                    db = new Db(sql, param);
+                }
+                else
+                {
+                    db = new Db(sql);
+                }
+
+                db.Execute();
+
+                Console.WriteLine("Executed query");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Cancel SQL");
+            }
         }
     }
 }
--- JobEngine/Db.cs
+++ JobEngine/Db.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace JobEngine
 {
-    class Db
+    public class Db
     {
         private string localDb = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Sam\Documents\GitHub\eal\jobboard\JobEngine\Database.mdf;Integrated Security=True";
+        private int numRows = 0;
 
         public string SqlQuery { get; set; }
 
         public Dictionary<string, object> Parameters { get; set; }
+
+        public int Rows
+        {
+            get
+            {
+                return numRows;
+            }
+        }
 
         public string ConnectData
         {
@@ -26,25 +38,164 @@ namespace JobEngine
             }
         }
 
-        
+
         public Db()
         {
 
         }
 
-        public void Execute()
+        public Db(string query)
         {
-
+            Query(query);
         }
 
-        public void Query()
+        public Db(string query, Dictionary<string, object> param)
         {
+            Query(query, param);
+        }
 
+        public void Query(string query)
+        {
+            SqlQuery = query;
+        }
+
+        public void Query(string query, Dictionary<string, object> param)
+        {
+            SqlQuery = query;
+            Parameters = param;
+        }
+
+        public bool Execute()
+        {
+            return ExecuteSql(GetCommandType());
+        }
+
+        public bool Execute(CommandType type)
+        {
+            return ExecuteSql(type);
         }
 
         public List<Dictionary<string, object>> Fetch()
         {
-            return new List<Dictionary<string, object>> { };
+            using (SqlConnection connect = new SqlConnection(ConnectData))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    try
+                    {
+                        command.CommandText = SqlQuery;
+                        command.CommandType = GetCommandType();
+                        command.Connection = connect;
+
+                        // Prepare Statement
+                        if (Parameters.Count != 0)
+                        {
+                            foreach (KeyValuePair<string, object> item in Parameters)
+                            {
+                                command.Parameters.AddWithValue("@" + item.Key, item.Value);
+                            }
+                        }
+
+                        // Open connection
+                        connect.Open();
+
+                        // Stored procedure
+                        if (command.CommandType == CommandType.StoredProcedure)
+                        {
+                            command.Prepare();
+                        }
+
+                        // Prepare Sql Data result
+                        using (SqlDataReader data = command.ExecuteReader())
+                        {
+                            // Prepare data list
+                            List<Dictionary<string, object>> dataList = new List<Dictionary<string, object>> { };
+
+                            // Reading SQL data
+                            while (data.Read())
+                            {
+                                // Prepare Associative array
+                                Dictionary<string, object> item = new Dictionary<string, object> { };
+
+                                // Adding SQL result in to array
+                                for (int i = 0; i < data.FieldCount; i++)
+                                {
+                                    item.Add(data.GetName(i), data.GetValue(i));
+                                }
+
+                                // Adding to data list
+                                dataList.Add(item);
+
+                                // Increase number of rows
+                                numRows++;
+                            }
+
+                            return dataList;
+                        }
+                    }
+                    catch
+                    {
+                        return new List<Dictionary<string, object>> { };
+                    }
+                }
+            }
+        }
+
+        private bool ExecuteSql(CommandType type)
+        {
+            using (SqlConnection connect = new SqlConnection(ConnectData))
+            {
+                using (SqlCommand command = new SqlCommand())
+                {
+                    try
+                    {
+                        // Open connection
+                        connect.Open();
+
+                        command.CommandText = SqlQuery;
+                        command.CommandType = type;
+                        command.Connection = connect;
+
+                        // Prepare Statement
+                        if (Parameters.Count != 0)
+                        {
+                            foreach (KeyValuePair<string, object> item in Parameters)
+                            {
+                                command.Parameters.AddWithValue("@" + item.Key, item.Value);
+                            }
+                        }
+
+                        // Stored procedure
+                        if (type == CommandType.StoredProcedure)
+                        {
+                            command.Prepare();
+                        }
+
+                        // Execute statement
+                        numRows = command.ExecuteNonQuery();
+
+                        return true;
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        
+
+        private CommandType GetCommandType()
+        {
+            if(Regex.IsMatch(SqlQuery.ToUpper(), "^(?:SELECT|INSERT INTO|UPDATE|DELETE FROM)"))
+            {
+                return CommandType.Text;
+            }
+            else
+            {
+                return CommandType.StoredProcedure;
+            }
         }
     }
 }
